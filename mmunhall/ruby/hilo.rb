79a9d3a38a118280cb0915f270f3d3c2acd07b3e
Mike Munhall <mike.munhall@canoe-ventures.com>
2012-01-05 13:04:57 -0700

    Ruby Day 1 Bonus

---
+++ mmunhall/ruby/hilo.rb
@@ -0,0 +1,12 @@
+rand = rand(10) + 1
+guess = -1
+
+while guess != rand
+    puts 'Enter a number between 1 and 10: '
+    guess = Integer(gets)
+    puts 'High. Guess again.' if guess > rand
+    puts 'Low. Guess again.' if guess < rand
+end
+
+puts 'Got it! All done.'
+
